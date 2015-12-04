b3fbe9b6376ce0b03d1c27bcb9153966639dfb3d
gncurran <gnc115@gmail.com>
2015-12-03 20:44:51 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,12 @@
-# Implement your object-oriented solution here!
+# Find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+
+class SmallestMultiple
+  def initialize(n)
+    @n = n
+  end
+
+  def lcm
+    array = 1.upto(@n).map {|x| x}.reverse
+    array.reduce(:lcm)
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,6 @@
-# Implement your procedural solution here!
+# Find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+
+def smallest_multiple(n)
+  array = 1.upto(n).map {|x| x}.reverse
+  array.reduce(:lcm)
+end
